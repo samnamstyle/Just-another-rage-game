4c8b2395a4b04825f2104603604a03bea5453170
samnamstyle <samnamstyle@users.noreply.github.com>
2015-01-16 15:51:05 -0800

    Added first question

--- Game.bat
+++ Game.bat
@@ -2,7 +2,7 @@
 //Menu
 :home
 cls
-echo Welcome to the game!
+echo Just Another Rage Game...
 echo Press T to enter the tutorial.
 echo Press P to play!
 set /p gamest=
@@ -11,8 +11,37 @@ if %gamest% == p goto play
 
 
 :tut
-
-
-:play
 cls
+echo This is a impossible quiz like game!
+echo Try To Win!
+echo Type in the letter you think is the answer!
+pause
+goto home
+
+:fail
+set /a life=%life%-1
+if %life% == 0 goto loss
+goto %q%
+:play
+set life=5
+cls
+//Make sure you set the question number!
+:q1
+set q=q1
+echo (1.)
+echo.
+echo Lives: %life%
+echo.
+echo Where is my thing?
+echo.
+echo.
+echo (A) Down Ma Pants
+echo (B) In my ear
+echo (C) In Ma BOOTY
+echo (D) Over There ->
+set /p answer=
+if %answer% == a goto fail 
+if %answer% == b goto fail
+if %answer% == c goto fail
+if %answer% == d goto q2
 
