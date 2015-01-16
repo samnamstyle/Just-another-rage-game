4f3283c29f9a033b07d10fde9b50ba10fdec774c
samnamstyle <samnamstyle@users.noreply.github.com>
2015-01-15 16:28:56 -0800

    Create Game.bat

---
+++ Game.bat
@@ -0,0 +1,15 @@
+@echo off
+//Menu
+:home
+cls
+echo Welcome to the game!
+echo Press T to enter the tutorial.
+echo Press P to play!
+set /p gamest=
+if %gamest% == t goto tut
+if %gamest% == p goto play
+
+
+:tut
+
+:play
